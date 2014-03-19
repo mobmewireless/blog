36dc1d34c9d00d586730a2c6b0f895bf86b8ced7
Vishnu Gopal <vishnu@mobme.in>
2014-03-19 18:15:26 +0530

    Reverting index.php to basic template from category template.

--- wp-content/themes/mobme/index.php
+++ wp-content/themes/mobme/index.php
@@ -16,65 +16,23 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<div id="content" class="site-content" role="main">
-<?php
-	$args = array( 'numberposts' => 10 );
-	$lastposts = get_posts( $args );
-	foreach($lastposts as $post): 
-		setup_postdata($post); 
-?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="entry-header">
-				<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-				<div class="entry-thumbnail">
-					<?php the_post_thumbnail(); ?>
-				</div>
-				<?php endif; ?>
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
 
-				<?php if ( is_single() ) : ?>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php else : ?>
-				<h1 class="entry-title">
-					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h1>
-				<?php endif; // is_single() ?>
+		<?php if ( have_posts() ) : ?>
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
 
-				<div class="entry-meta">
-					<?php twentythirteen_entry_meta(); ?>
-					<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- .entry-meta -->
-			</header><!-- .entry-header -->
+			<?php twentythirteen_paging_nav(); ?>
 
-			<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-			<div class="entry-summary">
-				<?php the_excerpt(); ?>
-			</div><!-- .entry-summary -->
-			<?php else : ?>
-			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
-				<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-			</div><!-- .entry-content -->
-			<?php endif; ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
 
-			<footer class="entry-meta">
-				<?php if ( comments_open() && ! is_single() ) : ?>
-					<div class="comments-link">
-						<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
-					</div><!-- .comments-link -->
-				<?php endif; // comments_open() ?>
-
-				<?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-					<?php get_template_part( 'author-bio' ); ?>
-				<?php endif; ?>
-			</footer><!-- .entry-meta -->
-		</article><!-- #post -->   
-<?php 
-	endforeach; 
-?>
-
-	</div><!-- #content -->
-</div><!-- #primary -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
